78cc324719c984b11e52a9b601f8d68f56e8bea1
musab benjadi <king_mos1993@yahoo.com>
2022-10-18 12:14:21 +0200

    add new repo

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+
+  <div class="container">
+    <!--title props in Header component-->
+    <Header title="Task Tracker" />
+    
+  </div>
+  
+</template>
+
+<script>
+
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+  data(){
+    task: []
+  },
+  created(){
+    this.task= [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'March 3st at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 1st at 11:00pm',
+        reminder: false,
+      },
+    ]
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 5px;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green" />
+    </header>
+</template>
+
+<script>
+
+import Button from './Button'
+import { stringLiteral } from '@babel/types';
+
+    export default {
+        name: 'Header',
+        props: {
+            title: String,
+        // other metod to bring data with default string
+        //     title: {
+        //         type: String,
+        //         default: 'header section',
+        // },
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
